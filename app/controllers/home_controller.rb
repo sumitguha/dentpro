d86f5d87a4a27b913e6bf81407f96edcff16278d
Glenn Rempe <glenn@rempe.us>
2009-12-15 12:53:21 -0800

    Use the numbersToWords plugin to show the # of franchises and states on a couple of pages.  Eliminates manual maintenance of text that shows those counts (in nice english words instead of Integers).

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,24 @@ class HomeController < ApplicationController
   end
 
   def who_we_are
+    # Used to display the number of locations and states in the 'Company Background' section.
+    locations = Location.find(:all, :order => 'state, name ASC')
+    states = []
+    locations.each do |loc|
+      states << loc.state unless loc.state.nil?
+    end
+
+    # degrade gracefully if we have not yet defined any locations in the DB
+    # Makes use of the numbersToWords plugin:
+    # http://github.com/accidental/numbersToWords
+    if states && states.length >= 1 && locations && locations.length >= 1
+      @locations = locations.length.to_english
+      @states = states.length.to_english
+    else
+      @locations = "many"
+      @states = "most of the western"
+    end
+
   end
 
   def what_we_do
