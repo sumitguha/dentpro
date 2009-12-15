86311a0fb990c4c3e302dcdd367bc21098bd4592
Glenn Rempe <glenn@rempe.us>
2009-12-15 13:09:10 -0800

    Seed to reset the admin user only in the dev environment.  Use 'heroku db:push' to push a full copy of the local dev DB to production (warning : overwriting the prod db).

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,8 @@
 if RAILS_ENV == 'development'
   User.destroy_all
   User.create(:email => 'admin@dentpro.com', :password => 'test', :password_confirmation => 'test')
-else
-  User.create(:email => 'admin@dentpro.com', :password => '3a9eb6fa508ce0c63ce92fa7ea', :password_confirmation => '3a9eb6fa508ce0c63ce92fa7ea')
+  u = User.find_by_email('admin@dentpro.com')
+  u.email_confirmed = true
+  u.save
 end
-
-u = User.find_by_email('admin@dentpro.com')
-u.email_confirmed = true
-u.save
 
